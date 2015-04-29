a68ed03fa70b79c7afb512365c5d132d04993277
Bitnami <bitnami@ubuntu14-generic-template-01.oit.duke.edu>
2015-04-28 22:35:48 -0400

    generated from execution files

--- functions.py
+++ functions.py
@@ -5,7 +5,7 @@ def calcM(Z,Kplus,sigmaX,sigmaA):
     """Save the matrix M so we won't need to calculate it again and again"""
     return np.linalg.inv(np.dot(Z[:,0:Kplus].T,Z[:,0:Kplus])+((sigmaX/sigmaA)**2)*np.identity(Kplus))
 
-def calcInverse(Z, M, i, k, val):
+def calcInverse_orig(Z, M, i, k, val):
     """Effective inverse calculation from Griffiths and Ghahramani (2005; Equations 51 to 54)
     M_(-i) = inv(inv(M) - zi.T * zi)"""
     M_i = M - np.dot(np.dot(M,Z[i,:].T),np.dot(Z[i,:],M))/(np.dot(np.dot(Z[i,:],M),Z[i,:].T)-1)
@@ -14,10 +14,12 @@ def calcInverse(Z, M, i, k, val):
     Inv = M
     return Inv
 
-def calcInverse_new(Z, M, i, k, val):
-    """New version to check: M_(-i) = inv(inv(M) - zi.T * zi)"""
+def calcInverse(Z, M, i, k, val):
+    """New version to check: M_(-i) = inv(inv(M) - zi.T * zi) and M = inv(inv(M_(-i)) + zi.T * zi)"""
+    M_i = np.linalg.inv(np.linalg.inv(M) - np.dot(Z[i,:].T,Z[i,:]))
     Z[i,k] = val
-    return np.linalg.inv(np.linalg.inv(M) - np.dot(Z[i,:].T,Z[i,:]))
+    M = np.linalg.inv(np.linalg.inv(M_i) + np.dot(Z[i,:].T,Z[i,:]))
+    return M
 
 def log_likelihood(X,Z,M,sigmaA,sigmaX,Kplus,N,D):  
     """Calculate the log-likelihood: P(X|Z,M,sigmaA,sigmaX,Kplus,N,D)"""  
